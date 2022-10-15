087c4b22ea99290534ef7fe38c2fc7797982c2a7
Nima Mashhadi <nima.mashhadi@snapp.cab>
2022-10-15 03:34:33 +0330

    add example repo

--- generator/template/generate_templates.py
+++ generator/template/generate_templates.py
@@ -2,6 +2,7 @@ from .tasks import make_job
 from .urls import make_url
 from .app import make_app
 from .views import make_view
+from .repository import make_repository
 
 
 def generate_templates(project_name: str, is_celery_enabled: bool) -> None:
@@ -16,3 +17,5 @@ def generate_templates(project_name: str, is_celery_enabled: bool) -> None:
     make_app(project_name)
 
     make_view(project_name)
+
+    make_repository(project_name)
