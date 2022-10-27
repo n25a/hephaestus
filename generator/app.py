39c03301ad636da086a1805963a9873b5d475d30
Nima Mashhadi <nima.mashhadi@snapp.cab>
2022-10-28 01:01:07 +0330

    add nats client to app

--- generator/app.py
+++ generator/app.py
@@ -1,12 +1,16 @@
-def make_app(project_name: str) -> None:
+def make_app(project_name: str, is_nats_enabled: bool) -> None:
     """
     Make app.
 
- :param project_name: project name in string type (e.g. my_project)
+    :param project_name: project name in string type (e.g. my_project)
+    :param is_nats_enabled: nat is enabled or not
    """
     with open(f"{project_name}/internals/app/app.py", "w") as app:
+        app.write("from typing import Optional\n")
+        app.write("from internals.nats import nats as internal_nats\n")
         app.write("from internals.app import ascii_art\n")
-        app.write("from internals.repositories.example import ExampleRepository\n\n\n")
+        app.write("from internals.repositories.example import ExampleRepository\n\n")
+        app.write("import nats\n\n\n")
 
         app.write("# ----------------------------------------------------------------\n")
         app.write("#                             Repository\n")
@@ -19,10 +23,18 @@ def make_app(project_name: str) -> None:
         app.write("# ----------------------------------------------------------------\n\n")
         app.write("class App:\n")
         app.write(f"    app_name: str = '{project_name}'\n")
-        app.write("    repository: Repository = Repository()\n\n")
+        app.write("    repository: Repository = Repository()\n")
 
-        app.write("    def __init__(self):\n")
-        app.write("        ascii_art.project_name()\n\n\n")
+        if is_nats_enabled:
+            app.write("    nats_client: Optional[nats.NATS] = None\n")
+
+        app.write("\n    def __init__(self):\n")
+        app.write("        ascii_art.project_name()\n")
+
+        if is_nats_enabled:
+            app.write("        self.nats_client = await internal_nats.create_client()\n")
+
+        app.write("\n\n")
 
         app.write("# ----------------------------------------------------------------\n")
         app.write("#                         public App variable\n")
